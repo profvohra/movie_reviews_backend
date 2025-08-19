541667633f0eef9b09786e5e3fe4c231a833665f
Rosemina Vohra <rosemina.vohra@gmail.com>
2025-08-18 20:54:23 -0400

    Chapter 12

--- dao/moviesDAO.js
+++ dao/moviesDAO.js
@@ -1,12 +1,15 @@
+import mongodb from "mongodb"
+const ObjectId = mongodb.ObjectId
+
 let movies
 
 export default class MoviesDAO {
   static async injectDB(conn) {
-    if(movies){ 
+    if (movies) {
       return
     } try {
       movies = await conn.db(process.env.MOVIEREVIEWS_NS).collection('movies')
-    } catch(e) {
+    } catch (e) {
       console.error(`unable to connect in MoviesDAO: ${e}`)
     }
   }
@@ -17,13 +20,13 @@ export default class MoviesDAO {
     moviesPerPage = 20,
   } = {}) {
     let query
-    if(filters) {
-      if("title" in filters) {
-        query = { $text: { $search: filters['title']}}
-      } else if("rated" in filters) {
-        query = { "rated": { $eq: filters['rated']}}
+    if (filters) {
+      if ("title" in filters) {
+        query = { $text: { $search: filters['title'] } }
+      } else if ("rated" in filters) {
+        query = { "rated": { $eq: filters['rated'] } }
+      }
     }
- }
     let cursor
     try {
       cursor = await movies
@@ -32,11 +35,49 @@ export default class MoviesDAO {
         .skip(moviesPerPage * page)
       const moviesList = await cursor.toArray()
       const totalNumMovies = await movies.countDocuments(query)
-      return {moviesList, totalNumMovies}
-    } catch(e) {
+      return { moviesList, totalNumMovies }
+    } catch (e) {
       console.error(`Unable to issue find command, ${e}`)
       console.error(e)
       return { moviesList: [], totalNumMovies: 0 }
     }
   }
+
+  static async getMovieById(id) {
+    try {
+      return await movies.aggregate([
+        {
+          $match: {
+            _id: ObjectId.createFromHexString(id),
+          }
+        },
+        {
+          $lookup:
+          {
+            from: 'reviews',
+            localField: '_id',
+            foreignField: 'movie_id',
+            as: 'reviews'
+          }
+        }
+      ]).next()
+    }
+    catch (e) {
+      console.error(`something went wrong in getMovieById: ${e}`)
+      throw e
+    }
+  }
+
+  static async getRatings() { 
+  let ratings = []
+  try {
+    ratings = await movies.distinct("rated")
+    return ratings
+  } catch(e) {
+    console.error(`unable to get ratings, ${e}`)
+    return ratings
+  }
+}
+
+
 }
